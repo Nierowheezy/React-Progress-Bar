7f936595d6afa915c610a67ea92fd7e70702ce28
Nierowheezy <olaniyiolabode99@yahoo.com>
2020-06-09 01:27:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Progress = ({ done }) => {
+  const [style, setStyle] = useState({});
+
+  setTimeout(() => {
+    const newStyle = {
+      opacity: 1,
+      width: `${done}%`
+    };
+
+    setStyle(newStyle);
+  }, 200);
+
+  return (
+    <div className="progress">
+      <div className="progress-done" style={style}>
+        {done}%
+      </div>
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <h1>React Progress Bar</h1>
+    <Progress done="90" />
+  </>
+);
+
+export default App;
